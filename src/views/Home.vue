efbe10ccec54a2caae8f54a784ceb92e3c170b03
TitikUniverse <semyon.titov.203@mail.ru>
2021-10-12 19:18:32 +0300

    Пофиксил связь с сервером

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,9 +3,12 @@
     <div class="row">
       <div class="col-sm-10">
         <h1>Заметки</h1>
-        <hr><br><br>
-        <button type="button" class="btn btn-success btn-sm">Добавить заметку</button>
-        <br><br>
+        <hr />
+        <br /><br />
+        <button type="button" class="btn btn-success btn-sm">
+          Добавить заметку
+        </button>
+        <br /><br />
         <table class="table table-hover">
           <thead>
             <tr>
@@ -17,49 +20,138 @@
           </thead>
           <tbody>
             <tr v-for="(note, index) in notes" :key="index">
-              <td>{{ note.Title }}</td>
-              <td>{{ note.Body }}</td>
-              <td>{{ note.CreateTime }}</td>
+              <td>{{ note.title }}</td>
+              <td>{{ note.body }}</td>
+              <td>{{ note.createTime }}</td>
               <td>
-                <button type="button" class="btn btn-warning btn-sm">Изменить</button>
-                <button type="button" class="btn btn-danger btn-sm">Удалить</button>
+                <button type="button" class="btn btn-warning btn-sm">
+                  Изменить
+                </button>
+                <button type="button" class="btn btn-danger btn-sm">
+                  Удалить
+                </button>
               </td>
             </tr>
           </tbody>
         </table>
       </div>
     </div>
+
+    <!-- <b-modal
+      ref="addBookModal"
+      id="book-modal"
+      title="Add a new book"
+      hide-footer
+    >
+      <b-form @submit="onSubmit" @reset="onReset" class="w-100">
+        <b-form-group
+          id="form-title-group"
+          label="Title:"
+          label-for="form-title-input"
+        >
+          <b-form-input
+            id="form-title-input"
+            type="text"
+            v-model="addBookForm.title"
+            required
+            placeholder="Enter title"
+          >
+          </b-form-input>
+        </b-form-group>
+        <b-form-group
+          id="form-author-group"
+          label="Author:"
+          label-for="form-author-input"
+        >
+          <b-form-input
+            id="form-author-input"
+            type="text"
+            v-model="addBookForm.author"
+            required
+            placeholder="Enter author"
+          >
+          </b-form-input>
+        </b-form-group>
+        <b-form-group id="form-read-group">
+          <b-form-checkbox-group v-model="addBookForm.read" id="form-checks">
+            <b-form-checkbox value="true">Read?</b-form-checkbox>
+          </b-form-checkbox-group>
+        </b-form-group>
+        <b-button type="submit" variant="primary">Submit</b-button>
+        <b-button type="reset" variant="danger">Reset</b-button>
+      </b-form>
+    </b-modal> -->
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   data() {
     return {
       notes: [],
+      addNoteForm: {
+        Title: '',
+        Body: '',
+        CreateTime: '',
+      },
     };
   },
   methods: {
     getNotes() {
-      const path = 'http://localhost:50898/api/Notes';
-      axios.get(path)
+      const path = "http://localhost:52362/api/Notes";
+      axios
+        .get(path)
         .then((res) => {
           this.notes = res.data;
         })
-        .catch((error) => {
+        .catch((err) => {
           // eslint-отключение следующей строки
-          console.error(error);
+          console.error(err);
         });
     },
+    // addNote(payload) {
+    //   const path = 'http://localhost:50898/api/Notes';
+    //   axios.post(path, payload)
+    //     .then(() => {
+    //       this.getNotes();
+    //     })
+    //     .catch((error) => {
+    //       // eslint-отключение следующей строки
+    //       console.log(error);
+    //       this.getNotes();
+    //     });
+    // },
+    // initForm() {
+    //   this.addNoteForm.Title = '';
+    //   this.addNoteForm.Body = '';
+    //   this.addNoteForm.CreateTime = '';
+    // },
+    // onSubmit(evt) {
+    //   evt.preventDefault();
+    //   this.$refs.addBookModal.hide();
+
+    //   const payload = {
+    //     Title: this.addNoteForm.Title,
+    //     Body: this.addNoteForm.Body,
+    //     CreateTime: this.addNoteForm.CreateTime,
+    //   };
+
+    //   this.addNote(payload);
+    //   this.initForm();
+    // },
+    // onReset(evt) {
+    //   evt.preventDefault();
+    //   this.$refs.addBookModal.hide();
+    //   this.initForm();
+    // },
   },
   created() {
     this.getNotes();
   },
-}
+};
 </script>
 
 <style>
-
 </style>
